4436f5e2e106877a2c0e2940476d9e3e45935b84
TheMulittle <murilogabardo@gmail.com>
2022-09-29 16:18:20 -0300

    Separetes properties in build file

--- src/main/java/com/mulittle/skeleton/backend/services/CompaniesService.java
+++ src/main/java/com/mulittle/skeleton/backend/services/CompaniesService.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
-import org.springframework.web.util.UriBuilder;
 
 import com.mulittle.skeleton.backend.model.Company;
 import com.mulittle.skeleton.backend.model.Urls;
@@ -15,13 +14,14 @@ public class CompaniesService {
   public static final String COMPANIES = "/companies";
   public static final String COMPANY = "/companies/{id}";
 
+  private static final WebTestClient baseClient = WebTestClient.bindToServer()
+    .baseUrl(Urls.BASE_URL)
+    .build();
+
   public ResponseSpec registerCompany(Company company) {
-    System.out.println(Urls.BASE_URL);
-   return WebTestClient.bindToServer()
-       .baseUrl(Urls.BASE_URL)
-        .build()
+    return baseClient
         .post()
-       .uri(COMPANIES)
+        .uri(COMPANIES)
         .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(company)
         .exchange();
