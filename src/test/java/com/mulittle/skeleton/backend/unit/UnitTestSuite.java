ccdebbef248e17bdce8ec5e27f14de16b7c1497a
TheMulittle <murilogabardo@gmail.com>
2023-02-19 17:05:28 -0300

    Fixed unit tests suite

--- src/test/java/com/mulittle/skeleton/backend/unit/UnitTestSuite.java
+++ src/test/java/com/mulittle/skeleton/backend/unit/UnitTestSuite.java
@@ -4,6 +4,6 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("com.mulittle.skeleton.backend.unit")
+@SelectPackages({ "com.mulittle.skeleton.backend.unit.parser", "com.mulittle.skeleton.backend.unit.context" })
 public class UnitTestSuite {
 }
